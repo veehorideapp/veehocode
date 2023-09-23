974d25df98c5f882d6cc2db20f8fd7d17b8c5f8d
ash hussain <ashtastic@Ashrifs-iMac.local>
2023-09-23 23:10:51 +0100

    push

--- checkuserreg.php
+++ checkuserreg.php
@@ -1,45 +1,34 @@
 <?php
-
-
-// PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:veehodbserver.database.windows.net,1433; Database = veeho_DB", "Veeho_Admin", "Mikhael1!");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $mobileNumber = isset($_POST['mobileNumber']) ? $_POST['mobileNumber'] : '';
 
-
-    // SQL query to fetch usernames
-   
-    $sql = "SELECT * FROM users WHERE username = :mobileNumber";
-
+    // SQL query to fetch user based on mobile number
+    $sql = "SELECT * FROM users WHERE mobileNumber = :mobileNumber";
 
     // Prepare and execute the SQL query
     $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':mobileNumber', $mobileNumber, PDO::PARAM_STR);
     $stmt->execute();
 
-    // Fetch all usernames as an associative array
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // Fetch the first matching user as an associative array
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if($result){
-
-    echo "UserFound";
-
+    if ($user) {
+        echo "UserFound";
     } else {
-
-    echo "NotFound";    
-
-    } 
-
+        echo "NotFound";
+    }
 } catch (PDOException $e) {
     // Handle database connection errors
     echo "Database Error: " . $e->getMessage();
     exit;
+} finally {
+    // Close the database connection
+    $conn = null;
 }
-
-// Close the database connection
-    $pdo = null;
-
 ?>
 
 
