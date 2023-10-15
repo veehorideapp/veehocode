4cb9c4f1e845b50136e8a3ec3bbfcced59fdd5cb
ash hussain <ashtastic@Ashrifs-iMac.local>
2023-10-15 23:44:44 +0100

    push

--- userregistration.php
+++ userregistration.php
@@ -15,31 +15,31 @@ try {
 
     // Hash the password using bcrypt with automatically generated salt
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
+    Echo password_hash;
     // SQL query to fetch user based on mobile number
-    $sql = "INSERT INTO users(username,email,password_hash,first_name,last_name,registration_date,last_login,is_active,role_id,user_type,mobile_number,verification_code) VALUES('$username','$email','$hashed_password','$forename','$surname',GETDATE(),GETDATE(),0,' ','RIDER','$userphoneno','$verificationcode')";
+ //   $sql = "INSERT INTO  users(username,email,password_hash,first_name,last_name,registration_date,last_login,is_active,role_id,user_type,mobile_number,verification_code) VALUES('$username','$email','$hashed_password','$forename','$surname',GETDATE(),GETDATE(),0,' ','RIDER','$userphoneno','$verificationcode')";
 
     // Prepare and execute the SQL query
-    $stmt = $conn->prepare($sql);
+ //   $stmt = $conn->prepare($sql);
    // $stmt->bindParam(':mobileNumber', $mobileNumber, PDO::PARAM_STR);
-    $stmt->execute();
+  //  $stmt->execute();
 
     // Fetch the first matching user as an associative array
   //  $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if ($stmt) {
-        echo "User Added";
-    } else {
-        echo "User Not Added";
-    }
+//    if ($stmt) {
+//        echo "User Added";
+//    } else {
+//        echo "User Not Added";
+ //   }
 } catch (PDOException $e) {
     // Handle database connection errors
     echo "Database Error: " . $e->getMessage();
     exit;
-} finally {
+//} finally {
     // Close the database connection
     $conn = null;
-}
+//}
 ?>
 
 
