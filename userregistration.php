ec72640a76ec26969343069bc12e8b0c49004952
ash hussain <ashtastic@Ashrifs-iMac.local>
2023-10-22 22:56:36 +0100

    push

--- userregistration.php
+++ userregistration.php
@@ -3,43 +3,43 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:veehodbserver.database.windows.net,1433; Database = veeho_DB", "Veeho_Admin", "Mikhael1!");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $password = isset($_POST['Password']) ? $_POST['Password'] : '';
-    $username = isset($_POST['Username']) ? $_POST['Username'] : '';
-    $forename = isset($_POST['Forename']) ? $_POST['Forename'] : '';
-    $surname = isset($_POST['Surname']) ? $_POST['Surname'] : '';
-    $usertype = isset($_POST['userType']) ? $_POST['UserType'] : '';
-    $userphoneno = isset($_POST['UserPhoneNo']) ? $_POST['UserPhoneNo'] : ''; 
-    $regtype = isset($_POST['RegType']) ? $_POST['RegType'] : '';
-    $verificationcode = isset($_POST['VerifCode']) ? $_POST['VerifCode'] : '';
-    $email = isset($_POST['EmailAdress']) ? $_POST['EmailAddress'] : '';
+    $password = isset($_GET['Password']) ? $_GET['Password'] : '';
+    $username = isset($_GET['Username']) ? $_GET['Username'] : '';
+    $forename = isset($_GET['Forename']) ? $_GET['Forename'] : '';
+    $surname = isset($_GET['Surname']) ? $_GET['Surname'] : '';
+    $usertype = isset($_GET['userType']) ? $_GET['UserType'] : '';
+    $userphoneno = isset($_GET['UserPhoneNo']) ? $_GET['UserPhoneNo'] : ''; 
+    $regtype = isset($_GET['RegType']) ? $_GET['RegType'] : '';
+    $verificationcode = isset($_GET['VerifCode']) ? $_GET['VerifCode'] : '';
+    $email = isset($_GET['EmailAdress']) ? $_GET['EmailAddress'] : '';
 
     // Hash the password using bcrypt with automatically generated salt
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
     Echo password_hash;
     // SQL query to fetch user based on mobile number
- //   $sql = "INSERT INTO  users(username,email,password_hash,first_name,last_name,registration_date,last_login,is_active,role_id,user_type,mobile_number,verification_code) VALUES('$username','$email','$hashed_password','$forename','$surname',GETDATE(),GETDATE(),0,' ','RIDER','$userphoneno','$verificationcode')";
+    $sql = "INSERT INTO  users(username,email,password_hash,first_name,last_name,registration_date,last_login,is_active,role_id,user_type,mobile_number,verification_code) VALUES('$username','$email','$hashed_password','$forename','$surname',GETDATE(),GETDATE(),0,' ','RIDER','$userphoneno','$verificationcode')";
 
     // Prepare and execute the SQL query
- //   $stmt = $conn->prepare($sql);
-   // $stmt->bindParam(':mobileNumber', $mobileNumber, PDO::PARAM_STR);
-  //  $stmt->execute();
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':mobileNumber', $mobileNumber, PDO::PARAM_STR);
+    $stmt->execute();
 
     // Fetch the first matching user as an associative array
   //  $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-//    if ($stmt) {
-//        echo "User Added";
-//    } else {
-//        echo "User Not Added";
- //   }
+    if ($stmt) {
+        echo "User Added";
+    } else {
+        echo "User Not Added";
+    }
 } catch (PDOException $e) {
     // Handle database connection errors
     echo "Database Error: " . $e->getMessage();
     exit;
-//} finally {
+} finally {
     // Close the database connection
     $conn = null;
-//}
+}
 ?>
 
 
