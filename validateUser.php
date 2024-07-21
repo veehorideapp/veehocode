23eaebdf25c57d53b45da0d7e10b0635d2c78ff5
ash hussain <ashtastic@ashs-MacBook-Air.local>
2024-07-21 10:19:02 +0100

    validateuser

--- validateUser.php
+++ validateUser.php
@@ -14,9 +14,12 @@ $password = isset($_POST['password']) ? $_POST['password'] : '';
 // This SQL statement selects ALL from the table 'Tariff'
 $sql = "SELECT 'Username','Password','Forename','Surname','UserType','EmailAddress','Registered','LoggedIn' FROM users
 WHERE Username='07898765476'";
+    
+    $sql1 = "SELECT * FROM users
+    WHERE Username=:username";
 
 // Prepare and execute the SQL query
-$stmt = $conn->prepare($sql);
+$stmt = $conn->prepare($sql1);
 //$stmt->bindParam(':username', $username, PDO::PARAM_STR);
 $stmt->execute();
 
