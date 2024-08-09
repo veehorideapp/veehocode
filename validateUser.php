ae2b2b3ed84fd2b8d55e6ff4dbfbaccf29dfd690
ash hussain <ashtastic@ashs-MacBook-Air.local>
2024-08-09 16:35:05 +0100

    validateuser

--- validateUser.php
+++ validateUser.php
@@ -26,7 +26,7 @@ $stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
     
 if ($user) {
-    $hash=$row("password_hash");
+    $hash=$user("password_hash");
     if(password_verify($password,$hash)) {
     echo json_encode($user);
        } else {
